4f2660addaf47cf41bbb0f446ea5deb41fe35b13
YariKar <kyaroslav03@gmail.com>
2026-02-09 16:43:50 +0300

    task 3

--- tasks/task_03/solution.go
+++ tasks/task_03/solution.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func fizzBuzz(n int) (string, error) {
-	// TODO
+
+	if n < 0 {
+		return "", fmt.Errorf("")
+	}
+	var result string = ""
+	if n%3 == 0 {
+		result += "Fizz"
+	}
+	if n%5 == 0 {
+		result += "Buzz"
+	}
+	if result == "" {
+		return strconv.Itoa(n), nil
+	} else {
+		return result, nil
+	}
 }
