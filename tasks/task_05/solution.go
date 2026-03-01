59637ab2b945b548a01fb97d38660678f8ee82ff
YariKar <kyaroslav03@gmail.com>
2026-03-01 13:49:55 +0300

    task 5

--- tasks/task_05/solution.go
+++ tasks/task_05/solution.go
@@ -1,3 +1,29 @@
 package main
 
-// TODO
+type Cache[K comparable, V any] struct {
+	data map[K]V
+}
+
+func NewCache[K comparable, V any](capacity int) *Cache[K, V] {
+	var m map[K]V
+	if capacity > 0 {
+		m = make(map[K]V, capacity)
+	}
+	return &Cache[K, V]{data: m}
+}
+
+func (c *Cache[K, V]) Set(key K, value V) {
+	if c.data == nil {
+		return
+	}
+	c.data[key] = value
+}
+
+func (c *Cache[K, V]) Get(key K) (V, bool) {
+	if c.data == nil {
+		var zero V
+		return zero, false
+	}
+	val, ok := c.data[key]
+	return val, ok
+}
